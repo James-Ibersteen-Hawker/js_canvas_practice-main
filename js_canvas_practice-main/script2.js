0e85185c05bc612ee13ed04340e3ded5c7fca1a2
James-Ibersteen-Hawker <526rserbinenko@frhsd.com>
2025-02-05 18:36:58 -0500

    fix equalize sorting problem, idk what it is

--- js_canvas_practice-main/script2.js
+++ js_canvas_practice-main/script2.js
@@ -1,72 +1,113 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let res = 100;
-
+let cRes = 3;
 let start = {
   x: 50,
   y: 500,
 };
 let ctrlP = {
-  x: 10,
-  y: 256,
+  x: 275,
+  y: 50,
 };
 let end = {
   x: 500,
-  y: 100,
+  y: 500,
 };
-let bezierPoints = [];
 let subWidth = 20;
 let subHeight = subWidth;
 function cubeBezier(p1, p2, p3) {
-  drawLines(p1, p2, p3);
+  let bezierPoints = [];
+  drawLines(p1, p2, p3, bezierPoints);
+  equalize(bezierPoints);
+  bezierPoints = sortPoints(bezierPoints);
+  draw(bezierPoints);
 }
-function drawLines(p1, p2, p3) {
+function drawLines(p1, p2, p3, bezierPoints) {
   ctx.fillStyle = "blue";
   ctx.fillRect(p1.x - 7, p1.y - 7, 14, 14);
   ctx.fillRect(p2.x - 7, p2.y - 7, 14, 14);
   ctx.fillRect(p3.x - 7, p3.y - 7, 14, 14);
   let p1Points = subDiv(p1, p2, res);
   let p2Points = subDiv(p2, p3, res);
-  inter(p1Points, p2Points, res);
+  inter(p1Points, p2Points, res, bezierPoints);
 }
-function subDiv(p1, p2, n) {
+function subDiv(p1, p2, n, t1, t2, sorting) {
   let points = [];
   for (let i = 1; i < n; i++) {
     let xCoord = Math.round(p1.x + ((p2.x - p1.x) / n) * i);
     let yCoord = Math.round(p1.y + ((p2.y - p1.y) / n) * i);
-    points.push({
-      x: xCoord,
-      y: yCoord,
-    });
+    if (!sorting) {
+      points.push({
+        x: xCoord,
+        y: yCoord,
+      });
+    } else {
+      let tDiff = ((t2 - t1) / n) * i;
+      points.push({
+        x: xCoord,
+        y: yCoord,
+        t: tDiff,
+      });
+    }
   }
   return points;
 }
-function inter(points1, points2, n) {
+function inter(points1, points2, n, bezierPoints) {
   for (let i = 0; i < n - 1; i++) {
     let iPoints = subDiv(points1[i], points2[i], n);
-    bezierPoints.push([iPoints[i].x, iPoints[i].y]);
+    bezierPoints.push({ x: iPoints[i].x, y: iPoints[i].y, t: i });
   }
 }
-function equalize() {}
-cubeBezier(start, ctrlP, end);
-start = {
-  x: 500,
-  y: 100,
-};
-ctrlP = {
-  x: 520,
-  y: 500,
-};
-end = {
-  x: 100,
-  y: 100,
-};
-cubeBezier(start, ctrlP, end);
-for (let i = 0; i < bezierPoints.length; i++) {
-  console.log("here!");
-  ctx.fillStyle = "red";
-  ctx.beginPath();
-  ctx.arc(bezierPoints[i][0], bezierPoints[i][1], subWidth / 2, 0, 2 * Math.PI);
-  ctx.fill();
-  ctx.stroke();
+function equalize(bezierPoints) {
+  for (let i = 0; i < bezierPoints.length; i++) {
+    if (i < res - 2) {
+      let d = Math.round(
+        Math.sqrt(
+          Math.pow(bezierPoints[i + 1].x - bezierPoints[i].x, 2) +
+            Math.pow(bezierPoints[i + 1].y - bezierPoints[i].y, 2)
+        ) / 3
+      );
+      let interPoints = subDiv(
+        bezierPoints[i],
+        bezierPoints[i + 1],
+        d,
+        bezierPoints[i].t,
+        bezierPoints[i + 1].t,
+        true
+      );
+      for (let j = 0; j < interPoints.length; j++) {
+        bezierPoints.push(interPoints[j]);
+      }
+    }
+  }
 }
+function draw(bezierPoints) {
+  for (let i = 0; i < bezierPoints.length; i++) {
+    setTimeout(
+      () => {
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.arc(
+          bezierPoints[i].x,
+          bezierPoints[i].y,
+          subWidth / 2,
+          0,
+          2 * Math.PI
+        );
+        ctx.fill();
+        ctx.stroke();
+      },
+      i * 10,
+      bezierPoints,
+      i
+    );
+  }
+}
+function sortPoints(bezierPoints) {
+  bezierPoints.sort((a, b) => {
+    return a.t - b.t;
+  });
+  return bezierPoints;
+}
+cubeBezier(start, ctrlP, end);
